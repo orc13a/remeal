9296d9e896c40f235b4344eec202b40f3e72d849
Oliver Christensen <orc13a@live.dk>
2021-04-08 15:57:45 +0200

    connection with DB

--- server/server.js
+++ server/server.js
@@ -6,3 +6,22 @@ const cors = require('cors');
 require('dotenv').config();
 
 const port = process.env.PORT || 8899;
+const dbConStr = process.env.DBCONSTR;
+
+const server = express();
+
+server.use(bodyParser.json({ limit: "30mb", extended: true }));
+server.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+server.use(cors());
+
+server.use('/', require('./routes/index'));
+
+mongoose.connect(dbConStr, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => server.listen(port, () => {
+        console.log(`\n==> Server listning on port: ${port}`);
+    }))
+    .catch((err) => {
+        console.log(err);
+    });
+
+mongoose.set('useFindAndModify', false);
