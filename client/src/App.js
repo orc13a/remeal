fa21ed7b4be50bc1af87d853b81565ce83719f17
Oliver Christensen <orc13a@live.dk>
2021-04-12 11:28:54 +0200

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 // import './App.css';
+import Navbar from './components/Navbar';
 
 // Dette er vores app og der som vil blive vist i din browser
 // Dette er et "root component"
 function App() {
   return (
     <div>
+      <Navbar />
       hello
     </div>
   );
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div className="titleName">
+                remeal
+            </div>
+            <div className="navButtonsContainer">
+
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
