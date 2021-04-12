9048facb4a8194b13f72ffb7ddcebeb45fd6ba16
Oliver Christensen <orc13a@live.dk>
2021-04-12 10:46:01 +0200

    font + comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 // import './App.css';
 
 // Dette er vores app og der som vil blive vist i din browser
+// Dette er et "root component"
 function App() {
   return (
     <div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+import './css/index.css';
 import App from './App';
 
 // Detter er lige som setup af vores app
