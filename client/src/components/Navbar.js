1c7129b5d84032136a91b8223d0dc2472c7455ed
Oliver Christensen <orc13a@live.dk>
2021-04-17 08:37:44 +0200

    router

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,7 +7,20 @@ const NavBar = () => {
                 remeal
             </div>
             <div className="navButtonsContainer">
+                <div className="">
+                    Status
+                </div>
+                <div className="navBtn">
+                    Mit køleskab
+                </div>
+            </div>
+            <div className="profilIconContainer">
+                <div className="profilIconUsername">
 
+                </div>
+                <div className="profilIcon">
+
+                </div>
             </div>
         </nav>
     );
