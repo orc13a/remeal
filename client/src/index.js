a550a2fc7562d423856416c767252e179afa5031
Oliver Christensen <orc13a@live.dk>
2021-04-23 09:41:08 +0200

    added npm packages

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, componse } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
+
+const store = createStore(reducers, componse(applyMiddleware(thunk)));
 
 // Detter er lige som setup af vores app
 ReactDOM.render(
