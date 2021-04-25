d794b21bd17d04feda3ed52cef5a5293086021aa
Oliver Christensen <orc13a@live.dk>
2021-04-25 10:51:26 +0200

    Update Login.js

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,28 +1,40 @@
-import { Container, Button, Grid, Typography, TextField } from '@material-ui/core/';
+import { makeStyles } from '@material-ui/core/styles';
+import { CardContent, Card, Container, Button, Grid, Typography, TextField } from '@material-ui/core/';
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 500,
+        minWidth: 300,
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 500,
+        marginBottom: 25,
+        textAlign: 'center'
+    }
+});
 
 function Login() {
+    const classes = useStyles();
+
     return (
-        <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justify="center"
-        style={{ minHeight: '100vh' }}
-        >
-        <Container maxWidth="xs">
-            <Grid item xl={true}>
-                <Typography>
-                    <form>
-                        <TextField type="email" fullWidth autoFocus id="outlined-basic" label="E-mail" variant="outlined" />
-                        <br/><br/>
-                        <TextField type="password" fullWidth id="outlined-basic" label="Adgangskode" variant="outlined" />
-                        <br/><br/>
-                        <Button size="large" fullWidth variant="contained" color="primary">Log ind</Button>
-                    </form>
-                </Typography>
-            </Grid>
-        </Container>
+        <Grid container spacing={0} alignItems="center" justify="center" style={{ minHeight: '100vh' }}>
+            <Container maxWidth="xs">
+                <Grid item xl={true}>
+                    <Card variant="outlined">
+                        <CardContent>
+                            <Typography className={ classes.title }>
+                                Log ind
+                            </Typography>
+                            <TextField type="email" fullWidth autoFocus id="outlined-basic" label="E-mail" variant="outlined" />
+                            <br/><br/>
+                            <TextField type="password" fullWidth id="outlined-basic" label="Adgangskode" variant="outlined" />
+                            <br/><br/>
+                            <Button size="large" fullWidth variant="contained" color="primary">Log ind</Button>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Container>
         </Grid>
     );
 }
